b50f33174fcd4573a54817f55ac726e632f9935e
BuildTools <unconfigured@null.spigotmc.org>
2017-05-31 23:06:33 -0700

    adding storms and giveItems

--- spigotmc/scriptcraft/plugins/storms.js
+++ spigotmc/scriptcraft/plugins/storms.js
@@ -1,23 +1,20 @@
 var _ = require('../modules/underscore.js');
 (function () {
-    var stormCount = 0;
-    var thisWorld = server.worlds.get(0);
-    var multiplier = (40 * 60) * 1000;
-    var timer = setInterval(timerHandler, multiplier);
+    var stormOn = false,
+        thisWorld = server.worlds.get(0),
+        interval = 600000,
+        timer = setInterval(timerHandler, interval);
 
-    console.log("underscore object is " + typeof _);
     function timerHandler () {
-        console.log("Multiplier is " + multiplier);
-        if (typeof thisWorld !== "undefined") {
-            if (stormCount % 5 === 0) {
-                console.log("SETSTORM=TRUE")
-                thisWorld.setStorm(true);
-            } else {
-                console.log("SETSTORM=FALSE")
-                thisWorld.setStorm(false);
-            }
+        if (stormOn) {
+            thisWorld.setStorm(false);
+            stormOn = false;
+            console.log("SETSTORM=FALSE")
+        } else {
+            console.log("SETSTORM=TRUE")
+            stormOn = true;
+            thisWorld.setStorm(true);
         }
-        stormCount += 1;
     }
 
 })();
