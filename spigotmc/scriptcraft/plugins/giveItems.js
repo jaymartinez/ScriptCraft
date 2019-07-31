0b659b7696a18596dc2eca9d6d0b76c6ece591a9
jay martinez <jmartinez308@hotmail.com>
2019-07-30 19:08:12 -0700

    snippets

--- spigotmc/scriptcraft/plugins/giveItems.js
+++ spigotmc/scriptcraft/plugins/giveItems.js
@@ -5,7 +5,9 @@ exports.giveItemsObject =
 
     function givePlayerStuff (event) {
         var player = event.player;
+        var playerInv = inv(player);
         var location = utils.getPlayerPos(player.name);
+        var i = 0;
         console.log(">>> World: %s, x: %f, y: %f, z: %f, yaw: %f, pitch: %f",
             location.world,location.x,location.y,location.z,location.yaw,location.pitch);
 
@@ -13,14 +15,27 @@ exports.giveItemsObject =
                 if (typeof items[prop] !== "function") {
                     continue;
                 }
-                //console.log("Name: " + prop + "\n" + "Value: " + items[prop] + "\n");
-                console.log(prop + " is " + typeof items[prop]);
-                inv(player).add(items[prop](10));
+
+                for (i = 1; i <= 64; i++) {
+                    if (playerInv.contains(items[prop](i))) {
+                        playerInv.remove(items[prop](i));
+                    }
+                }
             }
+            playerInv.add(items.diamondPickaxe(1));
     };
     
     events.blockBreak(function (event) {
-
+        var location = event.block.location;
+        var i = 0;
+        for (var j in event) {
+            console.log("!!!" + JSON.stringify(j));
+            if (j == "block") {
+                for (var k in j) {
+                    console.log("block[" + k + "]: " + j[k]);
+                }
+            }
+        }
     });
 
     events.playerJoin(_.bind(givePlayerStuff, this));
